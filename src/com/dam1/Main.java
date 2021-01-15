d3f934de60bfc11580507a9344a20eab2f0680cf
Jose Angel Sampere <jsamperevazquez@danielcastelao.org>
2021-01-15 19:44:17 +0100

    B

--- src/com/dam1/Main.java
+++ src/com/dam1/Main.java
@@ -4,5 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
 	// Código inicial para el ejercicio Git Ramas
+        //Hago un cambio en el código para hacer el commit B
     }
 }
